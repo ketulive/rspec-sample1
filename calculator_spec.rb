7d399e7d22a0a8cae9b90136291864830b823ce6
Yousuke Nii <yousuke.nii@gmail.com>
2015-09-12 14:35:39 +0900

    実装を追加する

--- calculator_spec.rb
+++ calculator_spec.rb
@@ -1,4 +1,14 @@
-describe "calculator" do
-	it { expect(true).to be true }
+def calc(expression)
+  nums = expression.scan(/\d+/)
+  result = 1
+  nums.each do |num|
+    result *= num.to_i
+  end
+  result
 end
 
+describe "calculator" do
+  it "15*5 = 75" do
+    expect(calc("15*5")).to eq 75
+  end
+end
