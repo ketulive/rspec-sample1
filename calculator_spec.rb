c0c3f5f1ff5f10f576ffef98e0e5497f821b87c1
Yousuke Nii <yousuke.nii@gmail.com>
2015-09-13 14:06:10 +0900

    テストと仮実装を追加する

--- calculator_spec.rb
+++ calculator_spec.rb
@@ -5,6 +5,16 @@ def calc(expression)
   end
 end
 
+def _calc(expressions)
+  pos = expressions.index("+")
+  op = expressions[pos]
+  left = expressions[pos-1].to_i
+  right = expressions[pos+1].to_i
+  res = left.send(op, right)
+  expressions[pos-1..pos+1] = res.to_s
+  expressions
+end
+
 describe "calculator" do
   it "15*5 = 75" do
     expect(calc("15*5")).to eq 75
@@ -24,5 +34,14 @@ describe "calculator" do
 
   it "30+15*5 = 225" do
     expect(calc("30+15*5")).to eq 225
+  end
+
+  it "30*15+5 = 600" do
+    pending("unimplemented")
+    expect(calc("30*15+5")).to eq 600
+  end
+
+  it "30*15+5 -> 30*20" do
+    expect(_calc(["30", "*", "15", "+", "5"])).to eq ["30", "*", "20"]
   end
 end
